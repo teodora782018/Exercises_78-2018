dc4dec94e3eb35767bb9273711838b3054d98fb3
teodora78 <78-2018@ftn.edu.rs>
2020-11-09 23:18:57 +0100

    Kreiran list box

--- Exercises_78-2018/Exercises_78-2018/Form1.cs
+++ Exercises_78-2018/Exercises_78-2018/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,5 +18,34 @@ namespace Exercises_78_2018
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                List<ExerciseResult> lista = new List<ExerciseResult>();
+            using (SqlConnection konekcija = new SqlConnection())
+            {
+                konekcija.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FacultyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                    konekcija.Open();
+                string q = "select* from ExerciseResults";
+                SqlCommand sqlCommand = new SqlCommand(q, konekcija);
+                using (SqlDataReader citac = sqlCommand.ExecuteReader())
+                {
+                    while (citac.Read())
+                    {
+                       
+                        ExerciseResult exerciseResult = new ExerciseResult();
+                        exerciseResult.Id = (int)citac.GetInt32(0);
+                        exerciseResult.StudentName = citac.GetString(1);
+                        exerciseResult.StudentIndex = citac.GetString(2);
+                        exerciseResult.Point = (int)citac.GetInt32(3);
+                        lista.Add(exerciseResult);
+                    }
+                }
+                }
+            listBox1.Items.Clear();
+                foreach (ExerciseResult exerciseResult1 in lista)
+                {
+                    listBox1.Items.Add(exerciseResult1.StudentIndex + ", " + exerciseResult1.Point);
+                }
+            }
     }
 }
